81374e525571e39111ddd0d230f4a0a3bd02e279
David Rio Deiros <driodeiros@gmail.com>
2014-04-26 18:03:57 -0500

    More question testing.

--- test/question.js
+++ test/question.js
@@ -14,4 +14,13 @@ assert(q.wrong().length === 1);
 q.wrong("1");
 assert(q.wrong().length === 2);
 
+q = client_logic.APP.question();
 
+q.question("2+2")
+ .correct("4")
+ .wrong("0")
+ .wrong("1");
+
+assert(q.question() === "2+2");
+assert(q.correct() === "4");
+assert(q.wrong().length === 2);
